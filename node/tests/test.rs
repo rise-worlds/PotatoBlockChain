31f88764e74cf41109cd7fc5738e2eb0e0692aa6
rise <rise.worlds@outlook.com>
2020-08-04 00:23:19 +0800

    sync

--- node/tests/test.rs
+++ node/tests/test.rs
@@ -1,7 +1,13 @@
+
 #[cfg(test)]
 mod tests {
+    pub fn add_one(x: i32) -> i32 {
+        x + 1
+    }
+    
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+        assert_eq!(3, add_one(2));
     }
 }
